7d74822c686d675473dcfdeb4c9f1b9f58e3ec54
Esmaeil Chitgar <ec.chitgar@gmail.com>
2024-10-20 20:35:05 +0330

    Update Autoformer_EncDec.py

--- layers/Autoformer_EncDec.py
+++ layers/Autoformer_EncDec.py
@@ -19,30 +19,34 @@ class my_Layernorm(nn.Module):
 
 class moving_avg(nn.Module):
     """
-    Moving average block to highlight the trend of time series
+    Exponential Moving Average (EMA) block to highlight the trend of time series
     """
-    def __init__(self, kernel_size, stride):
+    def __init__(self, kernel_size, smoothing_factor=0.1):
         super(moving_avg, self).__init__()
         self.kernel_size = kernel_size
-        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+        self.smoothing_factor = smoothing_factor
 
     def forward(self, x):
-        # padding on the both ends of time series
-        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        x = torch.cat([front, x, end], dim=1)
-        x = self.avg(x.permute(0, 2, 1))
-        x = x.permute(0, 2, 1)
-        return x
+        # Initialize EMA with the first value of the time series
+        ema = x[:, 0:1, :]
+        output = torch.zeros_like(x)
+
+        # Compute EMA for each time step
+        for t in range(1, x.size(1)):
+            ema = self.smoothing_factor * x[:, t:t+1, :] + (1 - self.smoothing_factor) * ema
+            output[:, t:t+1, :] = ema
+
+        return output
+
 
 
 class series_decomp(nn.Module):
     """
     Series decomposition block
     """
-    def __init__(self, kernel_size):
+    def __init__(self, kernel_size, smoothing_factor=0.1):
         super(series_decomp, self).__init__()
-        self.moving_avg = moving_avg(kernel_size, stride=1)
+        self.moving_avg = moving_avg(kernel_size, smoothing_factor=smoothing_factor)
 
     def forward(self, x):
         moving_mean = self.moving_avg(x)
